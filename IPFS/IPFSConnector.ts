95923a389a53012281ac0337d6cc1b45c7f2d4f0
Matus <mucka@netsearch.cz>
2019-12-14 19:56:22 +0100

    remove logging

--- IPFS/IPFSConnector.ts
+++ IPFS/IPFSConnector.ts
@@ -19,24 +19,24 @@ export default class IPFSconnector {
             IPFSconnector.instance.node = await IPFS.create(IPFSconnector.config);
             logger.info("node started!");
 
-            setInterval(async () => {
-                try {
-                    const peers = await IPFSconnector.instance.node.swarm.peers();
-                    console.log(`The node now has ${peers.length} peers.`);
-                } catch (err) {
-                    console.log("An error occurred trying to check our peers:", err);
-                }
-            }, 2000);
+            // setInterval(async () => {
+            //     try {
+            //         const peers = await IPFSconnector.instance.node.swarm.peers();
+            //         console.log(`The node now has ${peers.length} peers.`);
+            //     } catch (err) {
+            //         console.log("An error occurred trying to check our peers:", err);
+            //     }
+            // }, 2000);
             
-            // Log out the bandwidth stats every 4 seconds so we can see how our configuration is doing
-            setInterval(async () => {
-                try {
-                    const stats = await IPFSconnector.instance.node.stats.bw();
-                    console.log(`\nBandwidth Stats: ${JSON.stringify(stats, null, 2)}\n`);
-                } catch (err) {
-                    console.log("An error occurred trying to check our stats:", err);
-                }
-            }, 4000);
+            // // Log out the bandwidth stats every 4 seconds so we can see how our configuration is doing
+            // setInterval(async () => {
+            //     try {
+            //         const stats = await IPFSconnector.instance.node.stats.bw();
+            //         console.log(`\nBandwidth Stats: ${JSON.stringify(stats, null, 2)}\n`);
+            //     } catch (err) {
+            //         console.log("An error occurred trying to check our stats:", err);
+            //     }
+            // }, 4000);
         
 
         }
